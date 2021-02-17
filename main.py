b55801b25be66d58048b29a4da05b672f6f27f6d
Jacob Hands <jacob@gogit.io>
2021-02-17 07:16:38 +0000

    check res

--- main.py
+++ main.py
@@ -16,31 +16,32 @@ print('Starting main loop')
 while True:
     # DOWNLOAD THE FILE
     r = requests.get(url)
-    with open(file_name, 'wb') as outfile:
-        outfile.write(r.content)
-    dt = datetime.utcnow()
-    date_time_path = dt.strftime('%Y/%m/%d/%H/%Y-%m-%dT%H-%M-%S')
-    # 2021/02/17/02/2021-02-17T02-01-09
+    if r.ok:
+      with open(file_name, 'wb') as outfile:
+          outfile.write(r.content)
+      dt = datetime.utcnow()
+      date_time_path = dt.strftime('%Y/%m/%d/%H/%Y-%m-%dT%H-%M-%S')
+      # 2021/02/17/02/2021-02-17T02-01-09
 
-    # GET THE HASH
-    # BUF_SIZE is totally arbitrary, change for your app!
-    BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
-    sha1 = hashlib.sha1()
-    with open(file_name, 'rb') as f:
-        while True:
-            data = f.read(BUF_SIZE)
-            if not data:
-                break
-            sha1.update(data)
+      # GET THE HASH
+      # BUF_SIZE is totally arbitrary, change for your app!
+      BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+      sha1 = hashlib.sha1()
+      with open(file_name, 'rb') as f:
+          while True:
+              data = f.read(BUF_SIZE)
+              if not data:
+                  break
+              sha1.update(data)
 
-    # SAVE NEW VERSIONS TO B2 STORAGE
-    sha1str = sha1.hexdigest()  # This seems to give the string
-    if sha1str != db['grab_file_hash']:  # It's new! Save it to B2!
-        print("SHA1: {0}".format(sha1str))
-        db['grab_file_hash'] = sha1str
-        # Upload new version to b2
-        destination_path = 'TDTrackerMattZ/{0}_{1}'.format(
-            date_time_path, file_name)
-        bucket.upload_file(Filename=file_name, Key=destination_path)
+      # SAVE NEW VERSIONS TO B2 STORAGE
+      sha1str = sha1.hexdigest()  # This seems to give the string
+      if sha1str != db['grab_file_hash']:  # It's new! Save it to B2!
+          print("SHA1: {0}".format(sha1str))
+          db['grab_file_hash'] = sha1str
+          # Upload new version to b2
+          destination_path = 'TDTrackerMattZ/{0}_{1}'.format(
+              date_time_path, file_name)
+          bucket.upload_file(Filename=file_name, Key=destination_path)
 
     time.sleep(12)  # check every 12 seconds
