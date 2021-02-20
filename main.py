6c3a6ff71ec042e8baf2e4f2c794063a65a69a34
Jacob Hands <jacob@gogit.io>
2021-02-20 23:52:52 +0000

    skip weekends

--- main.py
+++ main.py
@@ -14,39 +14,44 @@ bucket = s3.Bucket('mattz-history')
 print('Starting main loop')
 # MAIN LOOP TO UPDATE FILE
 while True:
-    # DOWNLOAD THE FILE
-    try:
-      r = requests.get(url)
-      if r.ok:
-        with open(file_name, 'wb') as outfile:
-            outfile.write(r.content)
-        dt = datetime.utcnow()
-        date_time_path = dt.strftime('%Y/%m/%d/%H/%Y-%m-%dT%H-%M-%S')
-        # 2021/02/17/02/2021-02-17T02-01-09
+    # Skip weekends
+    dt = datetime.utcnow()
+    isWeekend = (dt.isoweekday() == 6 or dt.isoweekday() == 0)
+    if not isWeekend:
+        # DOWNLOAD THE FILE
+        try:
+            r = requests.get(url)
+            if r.ok:
+                with open(file_name, 'wb') as outfile:
+                    outfile.write(r.content)
+                date_time_path = dt.strftime('%Y/%m/%d/%H/%Y-%m-%dT%H-%M-%S')
+                # 2021/02/17/02/2021-02-17T02-01-09
 
-        # GET THE HASH
-        # BUF_SIZE is totally arbitrary, change for your app!
-        BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
-        sha1 = hashlib.sha1()
-        with open(file_name, 'rb') as f:
-            while True:
-                data = f.read(BUF_SIZE)
-                if not data:
-                    break
-                sha1.update(data)
+                # GET THE HASH
+                # BUF_SIZE is totally arbitrary, change for your app!
+                BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+                sha1 = hashlib.sha1()
+                with open(file_name, 'rb') as f:
+                    while True:
+                        data = f.read(BUF_SIZE)
+                        if not data:
+                            break
+                        sha1.update(data)
 
-        # SAVE NEW VERSIONS TO B2 STORAGE
-        sha1str = sha1.hexdigest()  # This seems to give the string
-        if sha1str != db['grab_file_hash']:  # It's new! Save it to B2!
-            print("SHA1: {0}".format(sha1str))
-            db['grab_file_hash'] = sha1str
-            # Upload new version to b2
-            destination_path = 'TDTrackerMattZ/{0}_{1}'.format(
-                date_time_path, file_name)
-            try:
-              bucket.upload_file(Filename=file_name, Key=destination_path)
-            except:
-              print('FAILED TO UPLOAD, OH WELL BETTER LUCK NEXT TIME!')
-    except:
-      print('general error')
-    time.sleep(12)  # check every 12 seconds
+                # SAVE NEW VERSIONS TO B2 STORAGE
+                sha1str = sha1.hexdigest()  # This seems to give the string
+                if sha1str != db['grab_file_hash']:  # It's new! Save it to B2!
+                    print("SHA1: {0}".format(sha1str))
+                    db['grab_file_hash'] = sha1str
+                    # Upload new version to b2
+                    destination_path = 'TDTrackerMattZ/{0}_{1}'.format(
+                        date_time_path, file_name)
+                    try:
+                        bucket.upload_file(Filename=file_name,
+                                           Key=destination_path)
+                    except:
+                        print(
+                            'FAILED TO UPLOAD, OH WELL BETTER LUCK NEXT TIME!')
+        except:
+            print('general error')
+        time.sleep(12)  # check every 12 seconds
